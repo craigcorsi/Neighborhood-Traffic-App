a9e60c793eaf5fe933d9cf45e3aa5dbc7d668709
Craig <corsi007@umn.edu>
2018-07-31 18:30:36 -0500

    start of graphs

---
+++ client/src/math/graph.js
@@ -0,0 +1,57 @@
+class Graph {
+    constructor() {
+        this.vertices = {};
+        this.edges = {};
+        this.numberOfVertices = 0;
+        this.numberOfEdges = 0;
+    }
+}
+
+Graph.prototype.addVertex = function(v) {
+    if (v in this.vertices) {
+        console.log(`The vertex ${v} has already been added.\n`)
+        return this; 
+    }
+    this.vertices[v] = {
+        "in-degree": 0,
+        "name": v,
+        "out-degree": 0,
+        "weight": 1
+    };
+    this.edges[v] = {};
+
+    // Update counts
+    this.numberOfVertices++;
+
+    return this;
+}
+
+Graph.prototype.addEdge = function(v, w) {
+    if (!(v in this.vertices) || !(w in this.vertices)) {
+        console.log("An edge cannot be built since the endvertices are not defined.\n");
+        return this;
+    } else {
+        this.edges[v][w] = {
+            "id": Math.random().toString(),
+            "pair": [v, w],
+            "source": v,
+            "sink": w,
+            "weight": 1
+        };
+
+        // Update counts
+        this.numberOfEdges++;
+        this.vertices[v]["out-degree"] += 1;
+        this.vertices[v]["in-degree"] += 1;
+
+        return this;
+    }
+}
+
+module.exports = Graph;
+
+var t3 = new Graph();
+t3 = t3.addVertex(1).addVertex(2).addVertex(3)
+.addEdge(1,2).addEdge(2,3).addEdge(1,3);
+
+console.log(t3);
