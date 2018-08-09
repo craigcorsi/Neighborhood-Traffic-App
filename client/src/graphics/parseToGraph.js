09a0804c1a18a4c81feb9b6951c7d30dc2db0a8d
Craig <corsi007@umn.edu>
2018-08-09 18:52:51 -0500

    refined xml parsing

--- client/src/graphics/parseToGraph.js
+++ client/src/graphics/parseToGraph.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 var StreetNetwork = require("../math/streetNetwork.js");
 var SandpileCore = require("../math/sandpileCore.js");
 
-text = fs.readFileSync("../temp_db/lombardStreet.osm");
+text = fs.readFileSync("../temp_db/lynlake.osm");
 const $ = cheerio.load(text, {
     withDomLvl1: true,
     normalizeWhitespace: true,
@@ -40,17 +40,38 @@ const population = generatePopulation(20);
 // });
 
 let counter = 0;
-
-$('way').each(function(i, element){
-    let isRoad = false;
-
-    $('tag').each(function(j, elt){
-        if ($(this).attr('k') == 'highway') {
-            counter++;
-        }
-    });
-});
-
 console.log(counter);
 
-// console.log(intersections);
+$('way tag').each(function (i, element) {
+    let isRoad = false;
+    if ($(this).attr("k") == "highway") {
+        // console.log($(this).parent().find("nd"));
+        $(this).parent().find("nd").each(function(j, elt){
+            var v = $(this).attr("ref");
+            intersections[v] = {}
+
+
+            // console.log($(this).next());
+            // console.log(`This node of the way has id ${$(this).attr("ref")}`);
+
+
+            if (j == $(this).parent().find("nd").length - 1) {
+                console.log("Heyo");
+            } else {
+                var w = $(this).next().attr("ref");
+                if (!(v in roads)) {
+                    roads[v] = {};
+                }
+                roads[v][w] = {
+                    "source": v,
+                    "sink": w
+                };
+            }
+        });
+        counter++;
+    }
+});
+
+// console.log(`Counter: ${counter}`);
+console.log(roads);
+console.log(intersections);
