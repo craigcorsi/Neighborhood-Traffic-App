84c956050fc0fa05747fe001d1b5cbd09d49168b
Philip Owusu <powusu6128@gmail.com>
2018-08-10 19:05:34 -0500

    adding files

---
+++ models/userQuery.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserQuerySchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  mapName: {
+    type: String,
+    required: true
+  },
+  boundingBox: {
+    max: String,
+    min:String,
+    long:String,
+    lat:String
+  },
+  avatar: {
+    type: String
+  },
+  created_date: {
+    type: Date,
+    default: Date.now
+  },
+  update_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = UserQuery = mongoose.model('Query', UserQuerySchema);
