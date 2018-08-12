8d2263e0b98fb2ca348c78c1ea5b25aa00b1e46d
Stacey Starken <staceystarken@gmail.com>
2018-08-11 19:04:44 -0500

    auth0 login changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,39 @@ import Header from './components/Header';
 import Main from './components/Main';
 //import Archive from './components/Archive';
 import Footer from './components/Footer';
+
+import Secret from "./components/Secret";
+import NotFound from "./components/NotFound";
+import Callback from "./components/Callback";
+
 class App extends Component {
   render() {
+    let mainComponent = ""
+   
+    switch (this.props.location)
+     {
+      case "":
+          mainComponent = <Main {...this.props} />;
+          break;
+      case "callback":
+          mainComponent = <Callback/>
+          break;
+      case "secret":
+          mainComponent = this.props.auth.isAuthenticated() ? <Secret {...this.props}/>:<NotFound/>;
+          break;
+      default:
+          mainComponent = <NotFound />;
+
+    }
+
     return (
       <div>
       <Header />
-      <Main />
+    
       <Footer />
+      <h1 className="App-title">Welcome to the Neighborhood, {this.props.name}.
+      </h1>
+      {mainComponent}
     </div>
     )
   }
