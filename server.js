90c2bce0438436dfc24755dd2474b3665d960c19
Craig <corsi007@umn.edu>
2018-08-12 18:27:12 -0500

    fix port 3000 in use error

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
 
 // Overpass API for retrieving map data
 const overpass = require("query-overpass");
@@ -20,39 +20,12 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 
-// // Define API routes here
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-// // Send every other request to the React app
-// // Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-
-
-
-// console.log("hello");
-
-// overpass('way(44.9454,-93.3000,44.9680,-93.2850);out;', function (error, response) {
-//   if (error) {
-//     console.log(error);
-//     return;
-//   }
-//   response = JSON.stringify(response, null, 2);
-//   // console.log(response);
-// });
-
-
 mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/Neighborhood-Traffic-db",
 );
 
 // Start the API server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log(`API Server now listening on PORT ${PORT}...`);
 });
