3430c84eca2a70fc53edb305466791ae5c2bdf1b
Philip Owusu <powusu6128@gmail.com>
2018-08-15 16:45:10 -0500

    accepting changes to the about page

--- client/src/components/pages/About/About.js
+++ client/src/components/pages/About/About.js
@@ -5,19 +5,6 @@ import { ButtonGroup, Button, FormGroup, InputGroup, FormControl, Grid, Row, Col
 class About extends React.Component {
     render() {
         return (
-<<<<<<< HEAD
-
-          <Grid>
-            <Row>
-                 <Col xs={12} md={8}>
-                            <p>Some text where we describe the app and our process</p>
-                 </Col>
-            </Row>
-         </Grid>
-            
-
-            
-=======
             <Grid>
                 <Row>
                     <Col xs={12} md={8}>
@@ -25,13 +12,12 @@ class About extends React.Component {
                         <p>Additional reading:</p>
                         <p>
                             <ul classname="reading">
-                           <li> <a href="http://nautil.us/issue/23/dominoes/the-amazing-autotuning-sandpile">"The Amazing Autotuning Sandpile" Ellenberg [2015] </a></li>
+                                <li> <a href="http://nautil.us/issue/23/dominoes/the-amazing-autotuning-sandpile">"The Amazing Autotuning Sandpile" Ellenberg [2015] </a></li>
                             </ul> 
                         </p>
                     </Col>
                 </Row>
             </Grid>
->>>>>>> c84946430d86479950d74c447014a232988939e7
         );
     }
 };
