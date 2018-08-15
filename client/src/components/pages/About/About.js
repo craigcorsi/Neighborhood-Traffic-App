c84946430d86479950d74c447014a232988939e7
Stacey Starken <staceystarken@gmail.com>
2018-08-14 20:24:41 -0500

    added url on About page

--- client/src/components/pages/About/About.js
+++ client/src/components/pages/About/About.js
@@ -9,6 +9,12 @@ class About extends React.Component {
                 <Row>
                     <Col xs={12} md={8}>
                         <p>Some text where we describe the app and our process</p>
+                        <p>Additional reading:</p>
+                        <p>
+                            <ul classname="reading">
+                           <li> <a href="http://nautil.us/issue/23/dominoes/the-amazing-autotuning-sandpile">"The Amazing Autotuning Sandpile" Ellenberg [2015] </a></li>
+                            </ul> 
+                        </p>
                     </Col>
                 </Row>
             </Grid>
