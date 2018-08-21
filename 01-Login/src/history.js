9e6cbbac37d32390fef8964132531f101ea0a5c7
Stacey Starken <staceystarken@gmail.com>
2018-08-21 17:54:07 -0500

    React is so fun

---
+++ 01-Login/src/history.js
@@ -0,0 +1,3 @@
+import createHistory from '../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/history/createBrowserHistory'
+
+export default createHistory()
---
+++ client/src/components/LogIn/LogIn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Background from "../../images/sandy.jpg"
+
+const LogIn = () => (
+  <div className="container-fluid">
+    <div className="row" style={{paddingTop:30}}>
+      <div className="col-12">
+        <h1 className="text-center" style={{}}>Log in to the neighborhood! </h1>
+      </div>
+  </div>
+  </div>
+);
+
+export default LogIn;
